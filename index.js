376745017c14ec981992498620bb6e8dbd2cb571
Alex Kibler <RKibler@ppg.com>
2021-07-29 07:54:11 -0400

    Added launch.json, added error tracking,  added file logging, cleaned up a lot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: __dirname + '/.env'});
 const tmi = require('tmi.js');
 const Discord = require('discord.js');
+const fs = require('fs');
 const DiscordClient = new Discord.Client();
 let discordBotReady = false;
 let discordChannel;
@@ -21,7 +22,7 @@ const client = new tmi.client(opts);
 
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on('connected', (addr, port) => console.log(`* Connected to ${addr}:${port}`));
 DiscordClient.on('ready', discordLoginHandler);
 
 
@@ -32,40 +33,35 @@ DiscordClient.login(process.env.DISCORD_AUTH_TOKEN);
 function discordLoginHandler() {
     console.log(`Logged in as ${DiscordClient.user.username}!`);
     discordChannel = DiscordClient.channels.cache.find(i => i.name === process.env.DISCORD_CHANNEL_NAME && i.guild.name === process.env.DISCORD_SERVER_NAME);
-    if (!discordChannel) {
-        throw 'Channel not found!'
+    if (discordChannel) {
+      console.log('logged into discord.  Channel fetched')
+      discordBotReady = true;    
+    } else {
+      errorHandler('Channel ' + process.env.DISCORD_CHANNEL_NAME + ' not found in server ' + process.env.DISCORD_SERVER_NAME)
+      throw 'Channel not found!';      
     }
-    console.log('logged into discord.  Channel fetched')
-    discordBotReady = true;    
 }
 
 
-// Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
   // Remove whitespace from chat message
   const message = msg.trim();
 
-  // If the command is known, let's execute it
   if (messageContainsLink(message)) {
-    // client.say(target, `Link det`);
-    console.log(`* Link detected from ${context.username}: `)
-    console.log(message);
+    const content = `${context.username}@${new Date().toLocaleString()}: ${message}\n`;
+    console.log(content);
     if (discordBotReady) {
-        //send message to discord
-        discordChannel.send(`${context.username}: ${message}`)
-    }
-  } else {
-      console.log(`${context.username}: ${message}`)
+        discordChannel.send(content)
+      }
+      //log message to file (if file doesn't exist, create it)
+      fs.writeFile('links.txt', content, { flag: 'a+'}, errorHandler);
   }
+}
+
+function errorHandler(errMsg) {
+  fs.writeFile('errors.txt', errMsg+'\n', {flag: 'a+'}, err => {});
 }
 
 function messageContainsLink(message) {
   return new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(message)
-}
-
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
 }
